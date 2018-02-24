c9ef601cdc92ecbf1c2aa86b2c06437abb1c774e
Pavel Korolev <dev@borodust.org>
2018-02-24 17:10:48 +0300

    Use cl:error

--- link.lisp
+++ link.lisp
@@ -2,4 +2,4 @@
 (bodge-blobs-support:link-system-foreign-libraries
  :glad-blob (asdf:system-relative-pathname :nanovg-blob #+x86-64 "x86_64" #+x86 "x86"
                                                         #-(or x86-64 x86)
-                                                        (error "Unsupported architecture")))
+                                                        (cl:error "Unsupported architecture")))
