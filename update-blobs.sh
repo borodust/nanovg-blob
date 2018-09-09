582a61d06c2e4dfb71ee256f45a625f4800d2a20
Pavel Korolev <dev@borodust.org>
2018-09-09 08:35:33 +0300

    Update blobs

--- update-blobs.sh
+++ update-blobs.sh
@@ -17,16 +17,22 @@ platforms=(["linux"]="so" ["osx"]="dylib" ["windows"]="dll")
 declare -A archs
 archs=(["x86_64"]="x86_64" ["i686"]="x86")
 
+declare -A gls
+gls=(["gl2"]="gl2" ["gl3"]="gl3")
+
 for arch in "${!archs[@]}" ; do
-    target_dir="$script_dir/${archs[$arch]}"
-    mkdir -p "$target_dir"
     for platform in "${!platforms[@]}" ; do
-        lib="lib$name.${platforms[$platform]}.bodged"
-        url="$repository_url/releases/download/$1/$lib-$arch-$platform-$1"
-        echo "Downloading $lib from $url"
-        if ! wget -O "$target_dir/$lib" -q "$url" ; then
-            echo "$lib download failed"
-            exit 1;
-        fi
+        for gl in "${!gls[@]}" ; do
+            target_dir="$script_dir/${archs[$arch]}/${gls[$gl]}"
+            mkdir -p "$target_dir"
+
+            lib="lib$name.${platforms[$platform]}.bodged"
+            url="$repository_url/releases/download/$1/$lib-$arch-$gl-$platform-$1"
+            echo "Downloading $lib from $url to $target_dir/$lib"
+            if ! wget -O "$target_dir/$lib" -q "$url" ; then
+                echo "$lib download failed"
+                exit 1;
+            fi
+        done
     done
 done
