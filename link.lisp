a934f585899bc48180bf359de6f3a15c75a00a62
Pavel Korolev <dev@borodust.org>
2018-10-02 23:13:05 +0300

    Fix glad blob linking

--- link.lisp
+++ link.lisp
@@ -1,5 +1,7 @@
 #+darwin
-(bodge-blobs-support:link-system-foreign-libraries
- :glad-blob (asdf:system-relative-pathname :nanovg-blob #+x86-64 "x86_64" #+x86 "x86"
-                                                        #-(or x86-64 x86)
-                                                        (cl:error "Unsupported architecture")))
+(let* ((gl-path (if (uiop:featurep :bodge-gl2) "gl2/" "gl3/"))
+       (relative-path (merge-pathnames gl-path #+x86-64 "x86_64/" #+x86 "x86/"
+                                               #-(or x86-64 x86)
+                                               (cl:error "Unsupported architecture"))))
+  (bodge-blobs-support:link-system-foreign-libraries
+   :glad-blob (asdf:system-relative-pathname :nanovg-blob relative-path)))
