57a9d7c1680df629b9e6b3e7e166fe747d07391c
Pavel Korolev <dev@borodust.org>
2019-10-14 22:12:53 +0300

    Fix linking

--- link.lisp
+++ link.lisp
@@ -1,8 +1,6 @@
 #+darwin
-(let* ((gl-path (if (uiop:featurep :bodge-gl2) "gl2/" "gl3/"))
-       (relative-path (merge-pathnames #+x86-64 "x86_64/" #+x86 "x86/"
-                                       #-(or x86-64 x86)
-                                       (cl:error "Unsupported architecture")
-                                       gl-path)))
-  (bodge-blobs-support:link-system-foreign-libraries
-   :glad-blob (asdf:system-relative-pathname :nanovg-blob relative-path)))
+(bodge-blobs-support:link-system-foreign-libraries
+ :glad-blob (asdf:system-relative-pathname
+             :nanovg-blob #+x86-64 "x86_64/" #+x86 "x86/"
+                          #-(or x86-64 x86)
+                          (cl:error "Unsupported architecture")))
